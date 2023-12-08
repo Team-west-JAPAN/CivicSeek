6442740a239042f72257ab766a5a4edf32d821aa
Yoshiyuki Kurose <moyashikuro1225@gmail.com>
2023-12-08 19:51:14 +0900

    deleted install.sh and added setup.sh

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ python3 -m venv venv # create virtual environment for python
 
 pip install -r requirements.txt # install required packages
 
-python3 regenerateSecretKey.py
+python3 regenerateSecretKey.py # regenerate secret key
 
 python3 ./manage.py migrate # migrate DB
 
-python3 manage.py loaddate ./sample_db.json
+python3 manage.py loaddate ./sample_db.json # add Contents of the database
 
-python3 manage.py runserver
+python3 manage.py runserver # activate server
