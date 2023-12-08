018be8accc2c32342a208a2cd6bae5726e9f6436
Yoshiyuki Kurose <moyashikuro1225@gmail.com>
2023-12-08 19:21:20 +0900

    deleted Dapp

--- ranking/urls.py
+++
@@ -1,8 +0,0 @@
-# coding:utf-8
-
-from django.urls import path
-from ranking.views import show_ranking
-
-urlpatterns = [
-    path('show_ranking/', show_ranking, name='show_ranking'),
-]
--- topics/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from topics.models import Topic,Comment,Tag
-
-# Register your models here.
-
-admin.site.register([Topic,Comment,Tag])
