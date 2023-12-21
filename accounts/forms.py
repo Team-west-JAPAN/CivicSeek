c10cc5a15b4a2ce99eb8e96afd2807a8bdecbbe0
Yoshiyuki Kurose <moyashikuro1225@gmail.com>
2023-12-21 23:28:20 +0900

    とりあえず改変前にコミット

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm, Authenti
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django import forms
+from accounts.models import Profile
 
 UserModel = get_user_model()
 
@@ -22,8 +23,17 @@ class CustomLoginForm(AuthenticationForm):
     username = forms.EmailField(label='Email')
 
 
-class ProfileEdit(UserChangeForm):
+class ProfileEditForm(forms.ModelForm):
     '''profileを編集するためのform
+    !!!: このformはまだサニタイジング処理を記述されていないため、メソッドをオーバーライドしてサニタイジングする必要がある
+    '''
+    class Meta:
+        model = Profile
+        fields = ('user', 'bio', 'is_student', 'birth_date')
+
+
+class CusomUserChangeForm(UserChangeForm):
+    '''UserChangeFormのサブクラス
     '''
     class Meta:
         model = UserModel
